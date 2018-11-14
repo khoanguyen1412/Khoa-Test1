65e3ccccbc5c9a84974de419cc4af485fc67f4c9
Nguyen Hong Khoa <17520087@gm.uit.edu.vn>
2018-11-14 22:09:32 +0700

    commit thứ 2

--- Test github/project 1/Program.cs
+++ Test github/project 1/Program.cs
@@ -17,6 +17,8 @@ namespace project_1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // change no.1
         }
     }
 }
